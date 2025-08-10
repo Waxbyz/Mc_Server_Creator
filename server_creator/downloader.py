f7270b4ec5c00200640f0fde795d3c99136251b4
Axon-waxby <waxby777@gmail.com>
2025-08-10 14:22:09 +0300

    Added SpigotDownloader and delete_directory, remove_readonly functions.

--- server_creator/downloader.py
+++ server_creator/downloader.py
@@ -1,39 +1,102 @@
-import requests
-from utils import create_directory
+import subprocess, requests
+from abc import ABC, abstractmethod
+from utils import create_directory, delete_directory
 
-HEADERS = {'User-Agent': "MinecraftServerCreator/1.0 (https://github.com/Waxbyz/Mc_Server_Creator)"}
+class Downloader(ABC):
+    HEADERS = {'User-Agent': "MinecraftServerCreator/1.0 (https://github.com/Waxbyz/Mc_Server_Creator)"}
 
-def papermc_downloader(version, name):
-    base_url = "https://api.papermc.io/v2/projects/paper"
-    versions_url = f"{base_url}/versions"
+    def __init__(self, version: str, name: str, direction: str):
+        self.version = version
+        self.name = name
+        self.direction = direction
+        self.latest_build = None
+        self.filename = None
+        self.save_dir = None
+        self.download_url = None
 
-    # versions = requests.get(base_url, headers=HEADERS).json()['versions']
-    builds = list(requests.get(f"{versions_url}/{version}/builds", headers=HEADERS).json()['builds'])[-1]
-    latest_build = builds['build']
-    filename = f"paper-{version}-{latest_build}.jar"
-    download_url = f"{base_url}/versions/{version}/builds/{latest_build}/downloads/{filename}"
+    @abstractmethod
+    def get_latest_build(self):
+        pass
 
-    save_dir = create_directory(f"C:/Users/User/AppData/Roaming/.mc_server_creator/servers/{name}")
-    save_dir = save_dir / filename
+    @abstractmethod
+    def prepare_save_path(self):
+        pass
 
-    print(f"Downloading {filename} to {save_dir}")
+    @abstractmethod
+    def download(self):
+        pass
 
-    jar_response = requests.get(download_url, headers=HEADERS)
-    jar_response.raise_for_status()
+    @abstractmethod
+    def run(self):
+        pass
 
-    with open(save_dir, "wb") as f:
-        f.write(jar_response.content)
+class PapermcDownloader(Downloader):
+    def __init__(self, version, name, direction):
+        super().__init__(version, name, direction)
+        self.base_url = "https://api.papermc.io/v2/projects/paper"
 
-def spigot_downloader(version, name):
-    pass
+    def get_latest_build(self):
+        versions_url = f"{self.base_url}/versions"
+        builds = list(requests.get(f"{versions_url}/{self.version}/builds", headers=self.HEADERS).json()['builds'])[-1]
+        latest_build = builds['build']
+        self.filename = f"paper-{self.version}-{latest_build}.jar"
+        self.download_url = f"{self.base_url}/versions/{self.version}/builds/{latest_build}/downloads/{self.filename}"
 
-def fabric_downloader(version, name):
-    pass
+    def prepare_save_path(self):
+        save_path = create_directory(f"{self.direction}/{self.name}")
+        self.save_dir = save_path / self.filename
 
-def forge_downloader(version, name):
-    pass
+    def download(self):
+        print(f"Downloading {self.name} from {self.download_url}")
+        jar_response = requests.get(self.download_url, headers=self.HEADERS)
+        jar_response.raise_for_status()
+        with open(self.save_dir, "wb") as f:
+            f.write(jar_response.content)
+        print(f"Saved {self.save_dir}")
 
-def neoforge_downloader(version, name):
-    pass
+    def run(self):
+        self.get_latest_build()
+        self.prepare_save_path()
+        self.download()
 
-papermc_downloader(version="1.12.2", name="paper-1.12.2")
+class SpigotDownloader(Downloader):
+    def __init__(self, version, name, direction):
+        super().__init__(version, name, direction)
+        self.base_url = "https://hub.spigotmc.org/jenkins/job/BuildTools"
+        self.save_path = None
+
+    def get_latest_build(self):
+        self.download_url = f"{self.base_url}/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+        self.filename = "BuildTools.jar"
+
+    def prepare_save_path(self):
+        self.save_path = create_directory(f"{self.direction}/{self.name}")
+        self.save_dir = self.save_path / self.filename
+
+    def download(self):
+        jar_response = requests.get(self.download_url, headers=self.HEADERS)
+        jar_response.raise_for_status()
+        with open(self.save_dir, "wb") as f:
+            f.write(jar_response.content)
+
+        cmd = ["java", "-jar", str(self.save_dir), "--rev", self.version]
+        subprocess.run(cmd, cwd=self.save_path, check=True)
+
+    def delete(self):
+        try:
+            delete_directory(f"{self.save_dir.parent}",
+                             f"spigot-{self.version}.jar")
+        except FileNotFoundError as e:
+            print(f"[!] Folder is not found {e}")
+
+    def run(self):
+        self.get_latest_build()
+        self.prepare_save_path()
+        self.download()
+        self.delete()
+
+
+paper = PapermcDownloader(version="1.21.1", name="paper-1.21.1", direction="C:/Users/User/AppData/Roaming/.mc_server_creator/servers")
+paper.run()
+spigot = SpigotDownloader(version="1.21.1", name="spigot-1.21.1", direction="C:/Users/User/AppData/Roaming/.mc_server_creator/servers")
+spigot.run()
--- server_creator/utils.py
+++ server_creator/utils.py
@@ -1,6 +1,31 @@
-import pathlib
+from pathlib import Path
+import shutil
+import os
+import stat
 
 def create_directory(directory):
-    directory = pathlib.Path(directory)
+    directory = Path(directory)
     directory.mkdir(parents=True, exist_ok=True)
     return directory
+
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+def delete_directory(directory, *filenames_to_keep):
+    directory = Path(directory)
+
+    if not directory.exists():
+        raise FileNotFoundError(f"Directory {directory} does not exist")
+
+    if not directory.is_dir():
+        raise NotADirectoryError(f"{directory} is not a directory")
+
+    for item in directory.iterdir():
+        if item.name in filenames_to_keep:
+            continue
+
+        if item.is_dir():
+            shutil.rmtree(item, onerror=remove_readonly)
+        else:
+            item.unlink()
